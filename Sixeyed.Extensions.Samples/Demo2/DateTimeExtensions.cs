1dca7c3bb99f39739e9d9bcf6359054c202be493
David Prause <praused@hotmail.com>
2017-09-27 17:11:09 -0400

    Segment 1, Demo 2, Piggybacking Namespaces

---
+++ Sixeyed.Extensions.Samples/Demo2/DateTimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace System //Piggybacking on the System namespace
+{
+    public static class DateTimeExtensions
+    {
+        /***************
+        public static string ToXmlDateTime(this DateTime dateTime)
+        {
+            //return XmlConvert.ToString(dateTime, XmlDateTimeSerializationMode.Utc);
+            ////Better to call the other ToXmlDateTime overload, less duplicate code...
+            //return dateTime.ToXmlDateTime(XmlDateTimeSerializationMode.Utc);
+            ////...or invoke the static method on the class.
+            //return ToXmlDateTime(dateTime, XmlDateTimeSerializationMode.Utc);
+            //Best way is to remove this method entirely and provide a default value for XmlDateTimeSerializationMode on the following method.
+            //This option makes mode an optional parameter.
+        }
+        ***************/
+
+        public static string ToXmlDateTime(this DateTime dateTime, XmlDateTimeSerializationMode mode = XmlDateTimeSerializationMode.Utc)
+        {
+            return XmlConvert.ToString(dateTime, mode);
+        }
+    }
+}
