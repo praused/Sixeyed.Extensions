528e97c37d008699bbe96f31f6e721d33cea0814
David Prause <praused@hotmail.com>
2017-09-27 11:33:08 -0400

    Segment 1, Demo 1, Extending System Types, Part 2

--- Sixeyed.Extensions.Tests/Demo1/LegacyExtensionsTests.cs
+++ Sixeyed.Extensions.Tests/Demo1/LegacyExtensionsTests.cs
@@ -65,5 +65,20 @@ namespace Sixeyed.Extensions.Tests.Demo1
             var dateTime = new DateTime(1020, 12, 31);
             Assert.AreEqual("0201231", dateTime.ToLegacyFormat());
         }
+
+        [TestMethod]
+        public void ToLegacyFormat_C21()
+        {
+            var dateTime = new DateTime(2013, 10, 31);
+            Assert.AreEqual("1131031", dateTime.ToLegacyFormat());
+        }
+
+        [TestMethod]
+        public void ToLegacyName()
+        {
+            var name = "Elton Stoneman";
+            Assert.AreEqual("STONEMAN, ELTON", name.ToLegacyFormat());
+        }
+
     }
 }
