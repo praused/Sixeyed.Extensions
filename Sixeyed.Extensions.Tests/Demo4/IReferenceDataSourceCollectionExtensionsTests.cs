e2a2be3a4357a289cd35f0e5163980b72e4acc18
David Prause <praused@hotmail.com>
2017-09-29 09:42:54 -0400

    Segment 1, Demo 4, Extending Collections, Array Extension

---
+++ Sixeyed.Extensions.Tests/Demo4/IReferenceDataSourceCollectionExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sixeyed.Extensions.Samples.Demo3;
+using Sixeyed.Extensions.Samples.Demo3.Impl;
+
+namespace Sixeyed.Extensions.Tests.Demo4
+{
+    [TestClass]
+    public class IReferenceDataSourceCollectionExtensionsTests
+    {
+        [TestMethod]
+        public void GetAllItemsByCode_Array()
+        {
+            var sources = new IReferenceDataSource[]
+            {
+                new SqlReferenceDataSource(),
+                new XmlReferenceDataSource(),
+                new ApiReferenceDataSource()
+            };
+            var items = sources.GetAllItemsByCode("xyz");
+            Assert.AreEqual(6, items.Count());
+        }
+    }
+}
